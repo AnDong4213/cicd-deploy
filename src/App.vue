7252f0773083d0b8eb99f088cf1e26165a4f0daa
赵安东 <zhaoandong4213@sina.com>
2024-09-28 10:12:50 +0800

    fix: 改动文件夹

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,16 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <img alt="Vue logo" src="./assets/xw.jpg">
+  <!-- <HelloWorld msg="Welcome to Your Vue.js App" /> -->
+  <h1>赵小玮</h1>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    // HelloWorld
   }
 }
 </script>
