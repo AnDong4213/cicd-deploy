062b1c31063417eb691e4b3fe39899ad9d747378
赵安东 <zhaoandong4213@sina.com>
2024-09-29 09:25:04 +0800

    fix: add

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <img alt="Vue logo" src="./assets/xw.jpg" style="width: 500px;">
+  <img alt="Vue logo" src="./assets/1.jpg" style="width: 500px" />
   <!-- <HelloWorld msg="Welcome to Your Vue.js App" /> -->
   <h5>Executing remote ssh commands.</h5>
 </template>
@@ -8,11 +8,11 @@
 // import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
     // HelloWorld
   }
-}
+};
 </script>
 
 <style>
